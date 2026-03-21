f9df07c55987a86b23bae55e14362520a7234681
Ben <oblingerb@gmail.com>
2026-03-21 18:23:00 -0400

    Add guns

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ const appId = typeof __app_id !== 'undefined' ? __app_id : 'svs-tactical-ledger'
 const DEFAULT_PRESETS = [
   "Pistol", "Combat Pistol", "AP Pistol", "Heavy Pistol", "SP45", "Pistol .50", "Taser",
   "Micro SMG", "SMG", "Combat PDW", "Gusenberg", "Tactical SMG", "Machine Pistol", "Mini SMG", 
-  "Assault Rifle", "Carbine Rifle", "Special Carbine", "Tactical Rifle", "Heavy Rifle",
+  "Assault Rifle", "Bullpup Rifle", "Carbine Rifle", "Special Carbine", "Tactical Rifle", "Heavy Rifle",
   "Combat MG", "Pump Shotgun",
   "9mm Ammo", "5.56 Ammo", "7.62 Ammo", "Shotgun Shells", "Taser cartridge",
   "Med Armor", "Joints", 
